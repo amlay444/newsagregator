fdd7b57453ec1c319dea3141d9bbc155688a7219
amlay444 <114230937+amlay444@users.noreply.github.com>
2024-11-18 17:12:59 +0300

    first commit

---
+++ components/AppBar.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Appbar} from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+const Header = () => {
+    return (
+        <SafeAreaProvider>
+        <Appbar.Header style={{marginTop:40, bckgroundColor:'silver'}}>
+            <Appbar.Content title="Home Screen"/>
+     </Appbar.Header>
+     </SafeAreaProvider>
+    );
+};
+
+export default Header
